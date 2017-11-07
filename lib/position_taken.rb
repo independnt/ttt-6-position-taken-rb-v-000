812fce398cc74bd973e9bfd830e949ef087ad6f6
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-11-07 19:55:31 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != " "
-  elsif board[index] != ""
-  elsif board[index] != nil
-  end
+  if board[index] != " " 
 end
